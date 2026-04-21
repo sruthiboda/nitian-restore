66fee3c04501cb42e4f43eedaed78881ab4a2191
Boda Sruthilaya <168336172+sruthiboda@users.noreply.github.com>
2026-04-21 18:45:06 +0530

    Updated frontend API URL

--- client/src/apicalls/axiosInstance.js
+++ client/src/apicalls/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export const axiosInstance = axios.create({
-    // baseURL: 'http://localhost:5000',
+    // baseURL: 'https://nitian-restore.onrender.com',
     headers : {
          authorization : `Bearer ${localStorage.getItem('token')}`
     }
